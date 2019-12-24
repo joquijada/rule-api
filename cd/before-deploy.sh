77f650292f3ce10c9fa29ed6cceb7fa7e959651d
Jose Quijada <joquijada2010@gmail.com>
2019-12-24 16:49:12 +0000

    feat(cd-cd): save

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_67304408949c_key -iv $encrypted_67304408949c_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_7092547da8b3_key -iv $encrypted_7092547da8b3_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --import --batch cd/codesigning.asc
 fi
